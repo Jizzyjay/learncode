f662518f086343f2e55ca00b33b97417eece63a1
Ekunola John Oluwatosin <ekunolajohn@gmail.com>
2022-07-04 09:43:37 +0100

    contentful

--- src/components/header.js
+++ src/components/header.js
@@ -2,18 +2,44 @@ import { Link } from "gatsby"
 import React from "react"
 import './Header.css'
 
-const Header = ({ siteTitle }) => (
-  <div className="Header">
-    <div className="HeaderGroup>">
-      <Link to="/"><img src={require('../images/logo-designcode.svg')} width="30" alt="" /></Link>
-      <Link to="/course">Courses</Link>
-      <Link to="/downnloads">Downloads</Link>
-      <Link to="/workshops">Workshops</Link>
-      <Link to="/buy"><button>Buy</button></Link>
-      
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hasScrolled: false
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll',
+    this.handleScroll)
+  }
+
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset
+
+    if (scrollTop > 50) {
+      this.setState({hasScrolled: true })
+    } else {
+      this.setState({ hasScrolled: false })
+    }
+  }
+
+  render() {
+    return(
+      <div className={this.state.hasScrolled ? 
+      'Header HeaderScrolled' : 'Header'}>
+        <div className="HeaderGroup>">
+          <Link to="/"><img src={require('../images/logo-designcode.svg')} width="30" alt="" /></Link>
+          <Link to="/course">Courses</Link>
+          <Link to="/downnloads">Downloads</Link>
+          <Link to="/workshops">Workshops</Link>
+          <Link to="/buy"><button>Buy</button></Link>
+        </div>
     </div>
-    Header
-  </div>
-)
+    )
+  }
+}
 
 export default Header
